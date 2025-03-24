e1ad79e5fb8018dea46496c89dbc9cf179689ff6
Evoreek <wh_961230@163.com>
2025-03-24 16:37:21 +0800

    提交桌面

---
+++ Assets/GlobalKeySound.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityRawInput;
+
+public class GlobalKeySound : MonoBehaviour {
+    public static GlobalKeySound Instance;
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    [Serializable]
+    public class KeySoundData {
+        public string sign;
+        public AudioClip audioClip;
+    }
+    public List<KeySoundData> keyPressSound; // 绑定的按键音效文件
+    public TMP_Dropdown _dropdown;
+    private AudioSource audioSource;
+    void Start() {
+        // 初始化音频组件
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+
+        // 初始化键盘监听
+        RawInput.WorkInBackground = true; // 添加此行
+        RawInput.InterceptMessages = false;
+        if (!RawInput.Start()) {// true表示启用后台输入检测
+            Debug.LogError("键盘钩子安装失败！请以管理员权限运行程序。");
+            return;
+        }
+        RawInput.OnKeyDown += OnKeyPressed;
+        Debug.Log("键盘监听已启动");
+
+        RefreshDropDown();
+    }
+
+    public void RefreshDropDown() {
+        _dropdown.ClearOptions();
+        _dropdown.AddOptions(keyPressSound.ConvertAll(x => x.sign));
+    }
+
+    private void OnKeyPressed(RawKey key) {
+        bool isKeyCode = IsKeyCodePress(key);
+        AudioClip clip = null;
+        foreach (var VARIABLE in keyPressSound) {
+            if (VARIABLE.sign == _dropdown.options[_dropdown.value].text) {
+                clip = VARIABLE.audioClip;
+            }
+        }
+        if (keyPressSound != null && isKeyCode) {
+            audioSource.PlayOneShot(clip);
+        }
+    }
+
+    private bool IsKeyCodePress(RawKey key) {
+        switch (key) {
+            case RawKey.Q:
+            case RawKey.W:
+            case RawKey.E:
+            case RawKey.R:
+            case RawKey.T:
+            case RawKey.Y:
+            case RawKey.U:
+            case RawKey.I:
+            case RawKey.O:
+            case RawKey.P:
+            case RawKey.A:
+            case RawKey.S:
+            case RawKey.D:
+            case RawKey.F:
+            case RawKey.G:
+            case RawKey.H:
+            case RawKey.J:
+            case RawKey.K:
+            case RawKey.L:
+            case RawKey.Z:
+            case RawKey.X:
+            case RawKey.C:
+            case RawKey.V:
+            case RawKey.B:
+            case RawKey.N:
+            case RawKey.M:
+            case RawKey.Space:
+            case RawKey.Return:
+                return true;
+        }
+
+        return false;
+    }
+
+    void OnDestroy() {
+        RawInput.Stop();
+        RawInput.OnKeyDown -= OnKeyPressed;
+    }
+}
